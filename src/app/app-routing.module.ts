5d58df78acd4a107f777b5d16fd8f2ded08276ad
jessica <jessica.c.m.cunha@hotmail.com>
2021-02-10 17:53:26 -0300

    prova jessica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { MainComponent } from './pages/main/main.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent
+},
+{
+  path: 'blocks',
+  component: BlocksComponent
+},
+{
+  path: 'cards',
+  component: CardsComponent
+},
+{
+  path: 'forms',
+  component: FormsComponent
+},
+{
+  path: 'pricing',
+  component: PricingComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { MainComponent } from './pages/main/main.component';
+import { BlocksComponent } from './pages/blocks/blocks.component';
+import { CardsComponent } from './pages/cards/cards.component';
+import { FormsComponent } from './pages/forms/forms.component';
+import { PricingComponent } from './pages/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    MainComponent,
+    BlocksComponent,
+    CardsComponent,
+    FormsComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
